37a2d0b628c9b986ea7add704341ee43da700e08
amontalvof <amontalvof92@gmail.com>
2022-07-18 12:16:20 -0500

    feat: Linked List Class

---
+++ data-structures/linked-list/index.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
+const l = new LinkedList();
+console.log('List is empty:', l.isEmpty());
+console.log('List size:', l.getSize());
