9d2356d025f0281d13e2224a2b3cfd9cc6ba917e
amontalvof <amontalvof92@gmail.com>
2022-07-19 09:26:13 -0500

    feat: Linked List Prepend

--- data-structures/linked-list/index.js
+++ data-structures/linked-list/index.js
@@ -18,8 +18,22 @@ class LinkedList {
     getSize() {
         return this.size;
     }
+
+    prepend(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
 }
 
 const l = new LinkedList();
 console.log('List is empty:', l.isEmpty());
 console.log('List size:', l.getSize());
+l.prepend(10);
+l.prepend(20);
+l.prepend(30);
