97c0ce64e8582c9cfe463863e8500f495bb359d3
amontalvof <amontalvof92@gmail.com>
2022-07-20 10:00:25 -0500

    feat: Linked List Print

--- data-structures/linked-list/index.js
+++ data-structures/linked-list/index.js
@@ -29,11 +29,28 @@ class LinkedList {
         }
         this.size++;
     }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log('List is empty');
+        } else {
+            let curr = this.head;
+            let list = '';
+            while (curr) {
+                list += `${curr.value} -> `;
+                curr = curr.next;
+            }
+            console.log(list);
+        }
+    }
 }
 
 const l = new LinkedList();
 console.log('List is empty:', l.isEmpty());
 console.log('List size:', l.getSize());
+l.print();
 l.prepend(10);
+l.print();
 l.prepend(20);
 l.prepend(30);
+l.print();
