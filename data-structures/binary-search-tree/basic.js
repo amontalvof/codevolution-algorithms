4a03fa13adff27861fe4ca29254f504961bad1b7
amontalvof <amontalvof92@gmail.com>
2022-08-17 09:38:16 -0500

    feat: Binary Search Tree Insert

--- data-structures/binary-search-tree/basic.js
+++ data-structures/binary-search-tree/basic.js
@@ -14,7 +14,40 @@ class BinarySearchTree {
     isEmpty() {
         return this.root === null;
     }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode.value < root.value) {
+            if (root.left === null) {
+                root.left = newNode;
+            } else {
+                this.insertNode(root.left, newNode);
+            }
+        } else {
+            if (root.right === null) {
+                root.right = newNode;
+            } else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
 console.log(bst.isEmpty());
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(13);
+bst.insert(17);
+bst.insert(2);
