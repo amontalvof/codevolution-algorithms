abe252a42b2bc4e837fd63513c6e75682e0f18b3
amontalvof <amontalvof92@gmail.com>
2022-10-12 09:25:34 -0500

    feat: finished recursion problems

---
+++ algorithms/recursion/capitalizeWords.js
@@ -0,0 +1,16 @@
+/*
+ * Write a recursive function called capitalizeWords.
+ * Given an array of words, return a new array containing each word capitalized.
+ */
+
+function capitalizeWords(array) {
+    if (array.length === 1) {
+        return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length - 1)[0].toUpperCase());
+    return res;
+}
+
+const words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
